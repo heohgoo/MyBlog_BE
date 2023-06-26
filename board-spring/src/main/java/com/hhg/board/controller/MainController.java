535f0dc43397821a2299a2e19d15f4d5e93c529a
Heo <gjgudrn1114@naver.com>
2023-06-26 18:49:59 +0900

    컨트롤러테스트

---
+++ board-spring/src/main/java/com/hhg/board/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.hhg.board.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(originPatterns = "http://localhost:3000") //CORS 에러 해결
+@RestController
+@RequestMapping("/")
+public class MainController {
+    @GetMapping("")
+    public String hello() {
+        return "Connection Successful";
+    }
+}
